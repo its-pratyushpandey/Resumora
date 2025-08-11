be2b9f5e5c261619e8adba5e1d4bd7f995848f69
Pratyush Kumar Pandey <2300030557@kluniversity.in>
2025-08-11 22:54:51 +0530

    Update popover.jsx

--- src/components/ui/popover.jsx
+++ src/components/ui/popover.jsx
@@ -3,7 +3,6 @@ import * as PopoverPrimitive from "@radix-ui/react-popover"
 import { cn } from "@/lib/utils"
 
 const Popover = PopoverPrimitive.Root
-
 const PopoverTrigger = PopoverPrimitive.Trigger
 const PopoverContent = React.forwardRef(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
   <PopoverPrimitive.Portal>
