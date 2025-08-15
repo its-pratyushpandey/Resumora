2ed8ff3b0c3e22f9edccf7f6f3e56a97fd7c4275
Pratyush Kumar Pandey <2300030557@kluniversity.in>
2025-08-15 23:29:13 +0530

    Update Header.jsx

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import { Button } from '../ui/button'
 import { Link } from 'react-router-dom'
 import { UserButton, useUser } from '@clerk/clerk-react'
-
-
-
 function Header() {
     const { user, isSignedIn } = useUser();
     return (
