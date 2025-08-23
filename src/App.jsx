4372bc4c3d08feb5edc76f8c73bb69d3db4c21cc
Pratyush Kumar Pandey <2300030557@kluniversity.in>
2025-08-24 02:27:21 +0530

    Remove unnecessary empty line in App component

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import { Navigate, Outlet } from 'react-router-dom'
 import { useUser } from '@clerk/clerk-react'
 import Header from './components/custom/Header'
 import { Toaster } from './components/ui/sonner'
-
 function App() {
   const [count, setCount] = useState(0)
   const {user,isLoaded,isSignedIn}=useUser();
