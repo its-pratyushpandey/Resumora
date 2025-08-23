fc82174b05f60b090c21fc8112d8c7b1feca7a4f
Pratyush Kumar Pandey <2300030557@kluniversity.in>
2025-08-24 02:26:40 +0530

    Update index.jsx

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -2,8 +2,7 @@ import Header from '@/components/custom/Header'
 import { UserButton } from '@clerk/clerk-react'
 import { AtomIcon, Edit, Share2 } from 'lucide-react'
 import React from 'react'
-function Home() {
-  
+function Home() { 
   return (
     <div>
       <Header/>
